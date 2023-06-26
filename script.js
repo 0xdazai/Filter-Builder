f3371aaa34f0aa6d49ed6d51acb2089300ee743b
Tiago <39215546+tiagovasc@users.noreply.github.com>
2023-06-26 10:28:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,40 +8,27 @@ document.getElementById('run-button').addEventListener('click', function() {
     
     var outputDiv = document.getElementById('output');
     outputDiv.innerHTML = '';  // clear previous output
-
-    var copiedCommands = JSON.parse(localStorage.getItem('copiedCommands')) || [];
-
-    allTerms.forEach(function(term, index) {
+    
+    allTerms.forEach(function(term) {
         if (isPhraseChecked) {
             var command = '/name-filters add phrase ' + term;
-            var copied = copiedCommands.includes(command) ? ' button-copy-copied' : '';
-            outputDiv.innerHTML += `<p><button class="button-copy${copied}" id="button-${index}" onclick="copyToClipboard('${command}', 'button-${index}')">Copy</button><span class="command-text">- ${command}</span></p>`;
+            outputDiv.innerHTML += `<p><button class="button-copy" data-filter="${command}" onclick="copyToClipboard(this)">Copy</button><span class="command-text">- ${command}</span></p>`;
         }
         if (isRegexChecked) {
             var command = '/name-filters add regex (?i)^' + term + '$';
-            var copied = copiedCommands.includes(command) ? ' button-copy-copied' : '';
-            outputDiv.innerHTML += `<p><button class="button-copy${copied}" id="button-${index+allTerms.length}" onclick="copyToClipboard('${command}', 'button-${index+allTerms.length}')">Copy</button><span class="command-text">- ${command}</span></p>`;
+            outputDiv.innerHTML += `<p><button class="button-copy" data-filter="${command}" onclick="copyToClipboard(this)">Copy</button><span class="command-text">- ${command}</span></p>`;
         }
     });
 });
 
-function copyToClipboard(text, buttonId) {
+function copyToClipboard(btn) {
+    var command = btn.dataset.filter;
     var tempInput = document.createElement("input");
-    tempInput.value = text;
+    tempInput.value = command;
     document.body.appendChild(tempInput);
     tempInput.select();
     document.execCommand("copy");
     document.body.removeChild(tempInput);
 
-    var buttonElement = document.getElementById(buttonId);
-    buttonElement.classList.add('button-copy-copied');
-
-    var copiedCommands = JSON.parse(localStorage.getItem('copiedCommands')) || [];
-    copiedCommands.push(text);
-    localStorage.setItem('copiedCommands', JSON.stringify(copiedCommands));
-}
-
-// When the page is refreshed, clear the local storage.
-window.onbeforeunload = function() {
-    localStorage.clear();
-};
+    // Mark this command as copied by changing the text color
+    var commandTexts = document.getElementsByClassName("command-text
